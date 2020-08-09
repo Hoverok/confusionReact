83fe0a44b7a7f76d3f4bc0d65dfd5874410f1eb9
Pavel Cikiliov <pavelchi91@gmail.com>
2020-08-09 14:36:22 +0300

    Fetch from server

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,15 +1,21 @@
-import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action) => {
-    switch(action.type) {
-        case ActionTypes.ADD_COMMENT: 
-            var comment = action.payload;
-            comment.id = state.length; //id= size of comments array
-            comment.date = new Date().toISOString();
-            //state is not mutated, creates a new value and returns it
-            return state.concat(comment);
-        default:
-            return state;
-    }
-}
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_COMMENTS:
+        //take state, create a new object from it and return new object. The original state does not mnutate
+      return {...state, errMess: null, comments: action.payload};
+
+    case ActionTypes.COMMENTS_FAILED:
+      return {...state, errMess: action.payload};
+
+    case ActionTypes.ADD_COMMENT:
+        var comment = action.payload;
+        comment.id = state.comments.length; //id= size of comments array
+        comment.date = new Date().toISOString();
+        return { ...state, comments: state.comments.concat(comment)};
+
+    default:
+      return state;
+  }
+};
